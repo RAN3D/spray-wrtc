6ca89a6f020215551bdb8a56169c0f55fff04b84
chat-wane <grumpy.chat.wane@gmail.com>
2016-04-06 18:04:57 +0200

    state change, still require to improve the neighbor module

--- example/main.js
+++ example/main.js
@@ -29,5 +29,13 @@ setTimeout(function(){
     var id2 = s3.connection(callbacks(s3, s1));
 }, 5000);
 
+// #3 connection state changes
+function changes(peer){
+    return function(state){
+        console.log('@'+peer + ' connection state '+ state);
+    };
+};
 
-
+s1.on('statechange', changes('s1'));
+s2.on('statechange', changes('s2'));
+s3.on('statechange', changes('s3'));
